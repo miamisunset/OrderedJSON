adb0b5f085dfec32e90d4f4fd6cec6360df40bdb
Alex Y. <96497214+miamisunset@users.noreply.github.com>
2026-05-23 23:04:25 -0400

    Remove SwiftFormat from Package.resolved

--- Package.swift
+++ Package.swift
@@ -20,7 +20,7 @@ let package = Package(
   ],
   dependencies: [
     // For OrderedDictionary - highly recommended
-    .package(url: "https://github.com/apple/swift-collections.git", from: "1.5.1"),
+    .package(url: "https://github.com/apple/swift-collections.git", from: "1.5.1")
   ],
   targets: [
     // Targets are the basic building blocks of a package, defining a module or a test suite.
